d7192fa5b38c350df09f55a773a69621e667a925
Thunderer9506 <srivastavashaurya225@gmail.com>
2024-12-29 17:40:42 +0530

    add PDF generation functionality using Jinja2 and pdfkit in main.py

--- main.py
+++ main.py
@@ -10,3 +10,11 @@ today_date = datetime.today().strftime("%d %b, %Y")
 context = {'my_name': my_name,'item1':item1,'item2':item2,'item3':item3,
             'today_date':today_date,}
 
+template_loader = jinja2.FileSystemLoader('./')
+template_emv = jinja2.Environment(loader=template_loader)
+
+template = template_emv.get_template('basic_template.html')
+output_text = template.render(context)
+
+config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
+pdfkit.from_string(output_text, 'pdf_generated.pdf', configuration=config,css="style.css")
