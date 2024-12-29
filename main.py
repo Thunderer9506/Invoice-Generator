e66c775246c99b8a39ee24b4ea694503e5fdf641
Thunderer9506 <srivastavashaurya225@gmail.com>
2024-12-29 22:00:31 +0530

    remove unused basic_template.html and pdf_generated.pdf; add new template.html and style.css for invoice generation

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import jinja2
 import pdfkit
 from datetime import datetime
 
-my_name = "Shaurya Srivastava"
-item1 = "TV"
-item2 = "Couch"
-item3 = "Washing machine"
-today_date = datetime.today().strftime("%d %b, %Y")
-context = {'my_name': my_name,'item1':item1,'item2':item2,'item3':item3,
-            'today_date':today_date,}
+# my_name = "Shaurya Srivastava"
+# item1 = "TV"
+# item2 = "Couch"
+# item3 = "Washing machine"
+# today_date = datetime.today().strftime("%d %b, %Y")
+# context = {'my_name': my_name,'item1':item1,'item2':item2,'item3':item3,
+#             'today_date':today_date,}
 
 template_loader = jinja2.FileSystemLoader('./')
 template_emv = jinja2.Environment(loader=template_loader)
 
 template = template_emv.get_template('basic_template.html')
-output_text = template.render(context)
+output_text = template.render()
 
 config = pdfkit.configuration(wkhtmltopdf=r"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe")
 pdfkit.from_string(output_text, 'pdf_generated.pdf', configuration=config,css="style.css")
