c4a58c68f26af466dc047b6a03e5c5245ccba50c
Thunderer9506 <srivastavashaurya225@gmail.com>
2024-12-29 17:14:46 +0530

    refactor template variables to lowercase and update context in main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import jinja2
 import pdfkit
+from datetime import datetime
+
+my_name = "Shaurya Srivastava"
+item1 = "TV"
+item2 = "Couch"
+item3 = "Washing machine"
+today_date = datetime.today().strftime("%d %b, %Y")
+context = {'my_name': my_name,'item1':item1,'item2':item2,'item3':item3,
+            'today_date':today_date,}
+
