101a58eb58375af29a8d955c42e8d9fd42130046
Thunderer9506 <srivastavashaurya225@gmail.com>
2024-12-30 15:15:11 +0530

    restructuring the tkinter using frames and ttk

--- main.py
+++ main.py
@@ -4,190 +4,208 @@ from datetime import datetime
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import ttk
 import docx
 
 class InvoiceAutomation:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Invoice Automation")
-        self.root.geometry("500x600")
+        self.root.geometry("600x600")
+
+        self.upperFrame = ttk.Frame(self.root)
+        self.upperFrame.place(x = 0, y = 0)
+        self.upperFrame.columnconfigure((0,1,2,3), weight=1)
+        self.upperFrame.rowconfigure((0,1,2), weight=1)
+
+        self.midFrame = ttk.Frame(self.root)
+        self.midFrame.place(relx = 0, rely = 1)
+        self.midFrame.columnconfigure((0,1,2,3), weight=1)
+        self.midFrame.rowconfigure((0,1,2), weight=1)
+
+        self.endFrame = ttk.Frame(self.root)
+        self.endFrame.place(relx = 0, rely = 2)
+        self.endFrame.columnconfigure((0,1,2,3), weight=1)
+        self.endFrame.rowconfigure((0,1,2), weight=1)
+
+        self.date_label = ttk.Label(self.upperFrame, text="Date")
+        self.invoice_label = ttk.Label(self.upperFrame, text="Invoice Number")
+
+        self.clientName_label = ttk.Label(self.upperFrame, text="Client Name")
+        self.clientAddress_label = ttk.Label(self.upperFrame, text="Client Address")
+        self.clientGST_label = ttk.Label(self.upperFrame, text="Client GST")
         
-        self.date_label = tk.Label(self.root, text="Date")
-        self.invoice_label = tk.Label(self.root, text="Invoice Number")
+        # self.description1_label = tk.Label(self.root, text="Description1")
+        # self.description2_label = tk.Label(self.root, text="Description2")
+        # self.description3_label = tk.Label(self.root, text="Description3")
+        # self.description4_label = tk.Label(self.root, text="Description4")
+        # self.description5_label = tk.Label(self.root, text="Description5")
 
-        self.clientName_label = tk.Label(self.root, text="Client Name")
-        self.clientAddress_label = tk.Label(self.root, text="Client Address")
-        self.clientGST_label = tk.Label(self.root, text="Client GST")
-        
-        self.description1_label = tk.Label(self.root, text="Description1")
-        self.description2_label = tk.Label(self.root, text="Description2")
-        self.description3_label = tk.Label(self.root, text="Description3")
-        self.description4_label = tk.Label(self.root, text="Description4")
-        self.description5_label = tk.Label(self.root, text="Description5")
+        # self.quantity1_label = tk.Label(self.root, text="Quantity1")
+        # self.quantity2_label = tk.Label(self.root, text="Quantity2")
+        # self.quantity3_label = tk.Label(self.root, text="Quantity3")
+        # self.quantity4_label = tk.Label(self.root, text="Quantity4")
+        # self.quantity5_label = tk.Label(self.root, text="Quantity5")
 
-        self.quantity1_label = tk.Label(self.root, text="Quantity1")
-        self.quantity2_label = tk.Label(self.root, text="Quantity2")
-        self.quantity3_label = tk.Label(self.root, text="Quantity3")
-        self.quantity4_label = tk.Label(self.root, text="Quantity4")
-        self.quantity5_label = tk.Label(self.root, text="Quantity5")
+        # self.rate1_label = tk.Label(self.root, text="Rate1")
+        # self.rate2_label = tk.Label(self.root, text="Rate2")
+        # self.rate3_label = tk.Label(self.root, text="Rate3")
+        # self.rate4_label = tk.Label(self.root, text="Rate4")
+        # self.rate5_label = tk.Label(self.root, text="Rate5")
 
-        self.rate1_label = tk.Label(self.root, text="Rate1")
-        self.rate2_label = tk.Label(self.root, text="Rate2")
-        self.rate3_label = tk.Label(self.root, text="Rate3")
-        self.rate4_label = tk.Label(self.root, text="Rate4")
-        self.rate5_label = tk.Label(self.root, text="Rate5")
+        # self.amount1_label = tk.Label(self.root, text="Amount1")
+        # self.amount2_label = tk.Label(self.root, text="Amount2")
+        # self.amount3_label = tk.Label(self.root, text="Amount3")
+        # self.amount4_label = tk.Label(self.root, text="Amount4")
+        # self.amount5_label = tk.Label(self.root, text="Amount5")
 
-        self.amount1_label = tk.Label(self.root, text="Amount1")
-        self.amount2_label = tk.Label(self.root, text="Amount2")
-        self.amount3_label = tk.Label(self.root, text="Amount3")
-        self.amount4_label = tk.Label(self.root, text="Amount4")
-        self.amount5_label = tk.Label(self.root, text="Amount5")
-
-        self.subtotal_label = tk.Label(self.root, text="Subtotal")
-        self.igst_label = tk.Label(self.root, text="IGST")
-        self.sgst_label = tk.Label(self.root, text="SGST")
-        self.cgst_label = tk.Label(self.root, text="CGST")
-        self.total_label = tk.Label(self.root, text="Total")
-        self.totalInword_label = tk.Label(self.root, text="Total In Word")
+        # self.subtotal_label = tk.Label(self.root, text="Subtotal")
+        # self.igst_label = tk.Label(self.root, text="IGST")
+        # self.sgst_label = tk.Label(self.root, text="SGST")
+        # self.cgst_label = tk.Label(self.root, text="CGST")
+        # self.total_label = tk.Label(self.root, text="Total")
+        # self.totalInword_label = tk.Label(self.root, text="Total In Word")
 
         #Create entry widget for every label 
-        self.date_entry = tk.Entry(self.root)
-        self.invoice_entry = tk.Entry(self.root)
+        self.date_entry = ttk.Entry(self.upperFrame,width=30)
+        self.invoice_entry = ttk.Entry(self.upperFrame,width=30)
 
-        self.clientName_entry = tk.Entry(self.root)
-        self.clientAddress_entry = tk.Entry(self.root)
-        self.clientGST_entry = tk.Entry(self.root)
+        self.clientName_entry = ttk.Entry(self.upperFrame,width=30)
+        self.clientAddress_entry = ttk.Entry(self.upperFrame,width=30)
+        self.clientGST_entry = ttk.Entry(self.upperFrame,width=30)
 
-        self.description1_entry = tk.Entry(self.root)
-        self.description2_entry = tk.Entry(self.root)
-        self.description3_entry = tk.Entry(self.root)
-        self.description4_entry = tk.Entry(self.root)
-        self.description5_entry = tk.Entry(self.root)
+        # self.description1_entry = tk.Entry(self.root)
+        # self.description2_entry = tk.Entry(self.root)
+        # self.description3_entry = tk.Entry(self.root)
+        # self.description4_entry = tk.Entry(self.root)
+        # self.description5_entry = tk.Entry(self.root)
 
-        self.quantity1_entry = tk.Entry(self.root)
-        self.quantity2_entry = tk.Entry(self.root)
-        self.quantity3_entry = tk.Entry(self.root)
-        self.quantity4_entry = tk.Entry(self.root)
-        self.quantity5_entry = tk.Entry(self.root)
+        # self.quantity1_entry = tk.Entry(self.root)
+        # self.quantity2_entry = tk.Entry(self.root)
+        # self.quantity3_entry = tk.Entry(self.root)
+        # self.quantity4_entry = tk.Entry(self.root)
+        # self.quantity5_entry = tk.Entry(self.root)
 
-        self.rate1_entry = tk.Entry(self.root)
-        self.rate2_entry = tk.Entry(self.root)
-        self.rate3_entry = tk.Entry(self.root)
-        self.rate4_entry = tk.Entry(self.root)
-        self.rate5_entry = tk.Entry(self.root)
+        # self.rate1_entry = tk.Entry(self.root)
+        # self.rate2_entry = tk.Entry(self.root)
+        # self.rate3_entry = tk.Entry(self.root)
+        # self.rate4_entry = tk.Entry(self.root)
+        # self.rate5_entry = tk.Entry(self.root)
 
-        self.amount1_entry = tk.Entry(self.root)
-        self.amount2_entry = tk.Entry(self.root)
-        self.amount3_entry = tk.Entry(self.root)
-        self.amount4_entry = tk.Entry(self.root)
-        self.amount5_entry = tk.Entry(self.root)
+        # self.amount1_entry = tk.Entry(self.root)
+        # self.amount2_entry = tk.Entry(self.root)
+        # self.amount3_entry = tk.Entry(self.root)
+        # self.amount4_entry = tk.Entry(self.root)
+        # self.amount5_entry = tk.Entry(self.root)
 
-        self.subtotal_entry = tk.Entry(self.root)
-        self.igst_entry = tk.Entry(self.root)
-        self.sgst_entry = tk.Entry(self.root)
-        self.cgst_entry = tk.Entry(self.root)
-        self.total_entry = tk.Entry(self.root)
-        self.totalInword_entry = tk.Entry(self.root)
+        # self.subtotal_entry = tk.Entry(self.root)
+        # self.igst_entry = tk.Entry(self.root)
+        # self.sgst_entry = tk.Entry(self.root)
+        # self.cgst_entry = tk.Entry(self.root)
+        # self.total_entry = tk.Entry(self.root)
+        # self.totalInword_entry = tk.Entry(self.root)
 
-        self.create_Button = tk.Button(self.root, text="Create Invoice", command=self.create_invoice)
-
-        padding_options = {'fill': 'x', 'expand':True, 'padx': 5, 'pady': 5} 
+        # self.create_Button = tk.Button(self.root, text="Create Invoice", command=self.create_invoice)
 
         # Now pack every widget
-        self.date_label.pack(padding_options)
-        self.date_entry.pack(padding_options)
+        # Create a canvas and a scrollbar
         
-        self.invoice_label.pack(padding_options)
-        self.invoice_entry.pack(padding_options)
-        
-        self.clientName_label.pack(padding_options)
-        self.clientName_entry.pack(padding_options)
-        
-        self.clientAddress_label.pack(padding_options)
-        self.clientAddress_entry.pack(padding_options)
-        
-        self.clientGST_label.pack(padding_options)
-        self.clientGST_entry.pack(padding_options)
-        
-        self.description1_label.pack(padding_options)
-        self.description1_entry.pack(padding_options)
-        
-        self.description2_label.pack(padding_options)
-        self.description2_entry.pack(padding_options)
-        
-        self.description3_label.pack(padding_options)
-        self.description3_entry.pack(padding_options)
-        
-        self.description4_label.pack(padding_options)
-        self.description4_entry.pack(padding_options)
-        
-        self.description5_label.pack(padding_options)
-        self.description5_entry.pack(padding_options)
-        
-        self.quantity1_label.pack(padding_options)
-        self.quantity1_entry.pack(padding_options)
-        
-        self.quantity2_label.pack(padding_options)
-        self.quantity2_entry.pack(padding_options)
-        
-        self.quantity3_label.pack(padding_options)
-        self.quantity3_entry.pack(padding_options)
-        
-        self.quantity4_label.pack(padding_options)
-        self.quantity4_entry.pack(padding_options)
-        
-        self.quantity5_label.pack(padding_options)
-        self.quantity5_entry.pack(padding_options)
-        
-        self.rate1_label.pack(padding_options)
-        self.rate1_entry.pack(padding_options)
-        
-        self.rate2_label.pack(padding_options)
-        self.rate2_entry.pack(padding_options)
-        
-        self.rate3_label.pack(padding_options)
-        self.rate3_entry.pack(padding_options)
-        
-        self.rate4_label.pack(padding_options)
-        self.rate4_entry.pack(padding_options)
-        
-        self.rate5_label.pack(padding_options)
-        self.rate5_entry.pack(padding_options)
-        
-        self.amount1_label.pack(padding_options)
-        self.amount1_entry.pack(padding_options)
-        
-        self.amount2_label.pack(padding_options)
-        self.amount2_entry.pack(padding_options)
-        
-        self.amount3_label.pack(padding_options)
-        self.amount3_entry.pack(padding_options)
-        
-        self.amount4_label.pack(padding_options)
-        self.amount4_entry.pack(padding_options)
-        
-        self.amount5_label.pack(padding_options)
-        self.amount5_entry.pack(padding_options)
-        
-        self.subtotal_label.pack(padding_options)
-        self.subtotal_entry.pack(padding_options)
-        
-        self.igst_label.pack(padding_options)
-        self.igst_entry.pack(padding_options)
-        
-        self.sgst_label.pack(padding_options)
-        self.sgst_entry.pack(padding_options)
-        
-        self.cgst_label.pack(padding_options)
-        self.cgst_entry.pack(padding_options)
-        
-        self.total_label.pack(padding_options)
-        self.total_entry.pack(padding_options)
-        
-        self.totalInword_label.pack(padding_options)
-        self.totalInword_entry.pack(padding_options)
-        
-        self.create_Button.pack(padding_options)
+
+        # Now use grid to place widgets in the scrollable frame
+        self.date_label.grid(row=0, column=0, padx=5, pady=5, sticky="w")
+        self.date_entry.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
+
+        self.invoice_label.grid(row=1, column=0, padx=5, pady=5, sticky="w")
+        self.invoice_entry.grid(row=1, column=1, padx=5, pady=5, sticky="ew")
+
+        self.clientName_label.grid(row=0, column=2, padx=5, pady=5, sticky="w")
+        self.clientName_entry.grid(row=0, column=3, padx=5, pady=5, sticky="ew")
+
+        self.clientAddress_label.grid(row=1, column=2, padx=5, pady=5, sticky="w")
+        self.clientAddress_entry.grid(row=1, column=3, padx=5, pady=5, sticky="ew")
+
+        self.clientGST_label.grid(row=2, column=2, padx=5, pady=5, sticky="w")
+        self.clientGST_entry.grid(row=2, column=3, padx=5, pady=5, sticky="ew")
+
+        # self.description1_label.grid(row=5, column=0, padx=5, pady=5, sticky="w")
+        # self.description1_entry.grid(row=5, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.description2_label.grid(row=6, column=0, padx=5, pady=5, sticky="w")
+        # self.description2_entry.grid(row=6, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.description3_label.grid(row=7, column=0, padx=5, pady=5, sticky="w")
+        # self.description3_entry.grid(row=7, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.description4_label.grid(row=8, column=0, padx=5, pady=5, sticky="w")
+        # self.description4_entry.grid(row=8, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.description5_label.grid(row=9, column=0, padx=5, pady=5, sticky="w")
+        # self.description5_entry.grid(row=9, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.quantity1_label.grid(row=10, column=0, padx=5, pady=5, sticky="w")
+        # self.quantity1_entry.grid(row=10, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.quantity2_label.grid(row=11, column=0, padx=5, pady=5, sticky="w")
+        # self.quantity2_entry.grid(row=11, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.quantity3_label.grid(row=12, column=0, padx=5, pady=5, sticky="w")
+        # self.quantity3_entry.grid(row=12, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.quantity4_label.grid(row=13, column=0, padx=5, pady=5, sticky="w")
+        # self.quantity4_entry.grid(row=13, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.quantity5_label.grid(row=14, column=0, padx=5, pady=5, sticky="w")
+        # self.quantity5_entry.grid(row=14, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.rate1_label.grid(row=15, column=0, padx=5, pady=5, sticky="w")
+        # self.rate1_entry.grid(row=15, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.rate2_label.grid(row=16, column=0, padx=5, pady=5, sticky="w")
+        # self.rate2_entry.grid(row=16, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.rate3_label.grid(row=17, column=0, padx=5, pady=5, sticky="w")
+        # self.rate3_entry.grid(row=17, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.rate4_label.grid(row=18, column=0, padx=5, pady=5, sticky="w")
+        # self.rate4_entry.grid(row=18, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.rate5_label.grid(row=19, column=0, padx=5, pady=5, sticky="w")
+        # self.rate5_entry.grid(row=19, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.amount1_label.grid(row=20, column=0, padx=5, pady=5, sticky="w")
+        # self.amount1_entry.grid(row=20, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.amount2_label.grid(row=21, column=0, padx=5, pady=5, sticky="w")
+        # self.amount2_entry.grid(row=21, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.amount3_label.grid(row=22, column=0, padx=5, pady=5, sticky="w")
+        # self.amount3_entry.grid(row=22, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.amount4_label.grid(row=23, column=0, padx=5, pady=5, sticky="w")
+        # self.amount4_entry.grid(row=23, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.amount5_label.grid(row=24, column=0, padx=5, pady=5, sticky="w")
+        # self.amount5_entry.grid(row=24, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.subtotal_label.grid(row=25, column=0, padx=5, pady=5, sticky="w")
+        # self.subtotal_entry.grid(row=25, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.igst_label.grid(row=26, column=0, padx=5, pady=5, sticky="w")
+        # self.igst_entry.grid(row=26, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.sgst_label.grid(row=27, column=0, padx=5, pady=5, sticky="w")
+        # self.sgst_entry.grid(row=27, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.cgst_label.grid(row=28, column=0, padx=5, pady=5, sticky="w")
+        # self.cgst_entry.grid(row=28, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.total_label.grid(row=29, column=0, padx=5, pady=5, sticky="w")
+        # self.total_entry.grid(row=29, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.totalInword_label.grid(row=30, column=0, padx=5, pady=5, sticky="w")
+        # self.totalInword_entry.grid(row=30, column=1, padx=5, pady=5, sticky="ew")
+
+        # self.create_Button.grid(row=31, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
 
         self.root.mainloop()
 
